fdf8a3afcdd11b45ff0f7a71fc6e81da326deae6
fahmed8383 <fahmed4030@gmail.com>
2021-12-01 18:29:32 -0500

    Working directory changed

--- conky.sh
+++ conky.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conky -c ~/.conky/Manjaro-Conky-Files/cpu_ram.conf &
-conky -c ~/.conky/Manjaro-Conky-Files/clock.conf &
-conky -c ~/.conky/Manjaro-Conky-Files/twitch.conf &
-conky -c ~/.conky/Manjaro-Conky-Files/stocks.conf
+conky -c $(dirname $0)/cpu_ram.conf &
+conky -c $(dirname $0)/clock.conf &
+conky -c $(dirname $0)/twitch.conf &
+conky -c $(dirname $0)/stocks.conf
